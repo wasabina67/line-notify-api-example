9082786cb254f3802960f4592d9da900cab27d4c
wasabina67 <wasabina67@gmail.com>
2024-08-14 18:39:46 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,5 +1,19 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
 def main():
-    pass
+    load_dotenv(override=True)
+    access_token = os.getenv("ACCESS_TOKEN")
+
+    url = "https://notify-api.line.me/api/notify"
+    headers = {"Authorization": f"Bearer {access_token}"}
+    data = {"message": "message"}
+    files = {"imageFile": open("./images/sample.jpg", "rb")}
+
+    requests.post(url=url, headers=headers, data=data, files=files)
 
 
 if __name__ == "__main__":
